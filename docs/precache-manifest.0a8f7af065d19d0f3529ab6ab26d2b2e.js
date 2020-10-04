da623bb9c23f89243f47dac8c94b37af3263751d
abhchand <me@abhchand.me>
2020-10-04 01:41:40 -0400

    Initial build

---
+++ docs/precache-manifest.0a8f7af065d19d0f3529ab6ab26d2b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae2342e75e2d3cace418c303e2a8120",
+    "url": "/languages/index.html"
+  },
+  {
+    "revision": "94f79a74291f50061dd7",
+    "url": "/languages/static/css/main.ca55a9e1.chunk.css"
+  },
+  {
+    "revision": "d9ddebb234ca7431c4ca",
+    "url": "/languages/static/js/2.f2d5f59f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/languages/static/js/2.f2d5f59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f79a74291f50061dd7",
+    "url": "/languages/static/js/main.7d03c894.chunk.js"
+  },
+  {
+    "revision": "1f31c8ee065a472e6189",
+    "url": "/languages/static/js/runtime-main.2806b5c6.js"
+  }
+]);
