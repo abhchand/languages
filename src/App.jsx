3291ca2873796b81211faa00d93d36c2c0d76d51
abhchand <me@abhchand.me>
2020-10-04 16:33:52 -0400

    Add `<TableOfLetters />` component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Flashcards from './Pages/Flashcards/Flashcards';
+import TableOfLetters from './Pages/TableOfLetters/TableOfLetters';
 
 class App extends React.Component {
   render() {
@@ -11,7 +12,8 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/languages/" exact component={Home} />
-          <Route path="/languages/:language/letters/flashcards" component={Flashcards} />
+          <Route path="/languages/:language/letters" exact component={TableOfLetters} />
+          <Route path="/languages/:language/letters/flashcards" exact component={Flashcards} />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/Pages/TableOfLetters/TableOfLetters.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import fetchData from '../../shared/fetch-data';
+
+import './TableOfLetters.css';
+
+class TableOfLetters extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.fetchData = fetchData.bind(this);
+
+    this.language = props.match.params.language;
+
+    this.state = {
+      cards: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <h1>Loading...</h1>;
+    }
+
+    return (
+      <div class="content">
+        <table>
+          <tr>
+            <th>Letter</th>
+            <th>Pronunciation</th>
+            <th>Example</th>
+          </tr>
+
+          {
+            this.state.cards.map(card => {
+              return (
+                <tr>
+                  <th>{card.char}</th>
+                  <th>{card.pronounce}</th>
+                  <th>{card.example}</th>
+                </tr>
+              );
+            })
+          }
+        </table>
+      </div>
+    );
+  }
+}
+
+export default TableOfLetters;
